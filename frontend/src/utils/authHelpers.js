36ae367cc26ae3e40ab226e24e9fe0311160fd25
Tuoyoesimaje <esimajetuoyo71@gmail.com>
2025-09-17 13:10:39 +0100

    ok

--- frontend/src/utils/authHelpers.js
+++ frontend/src/utils/authHelpers.js
@@ -1,2 +1,2 @@
-export const API_ROOT = (localStorage.getItem('sa_api_root') || 'http://localhost:3000');
+export const API_ROOT = (localStorage.getItem('sa_api_root') || 'https://alfred-ai-73gj.onrender.com');
 export const AUTH_TOKEN_KEY = 'sa_auth_token';
